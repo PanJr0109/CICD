a62f3d2b9e6750f73266897f8ec3f36423e57aaa
20120068 <phanduy0000123@gmail.com>
2024-12-11 23:21:34 +0700

    met r do

--- src/components/Cards/CardProfile.js
+++ src/components/Cards/CardProfile.js
@@ -42,7 +42,7 @@ export default function CardProfile() {
           </div>
           <div className="text-center mt-12">
             <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
-              Phan Duyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+              PDDDDDDDDDDd
             </h3>
             <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
               <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>{" "}

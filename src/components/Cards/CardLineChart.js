2d98b402390f10347da1cda7ef17d605b900e22c
20120068 <phanduy0000123@gmail.com>
2024-12-12 01:01:13 +0700

    change cardline

--- src/components/Cards/CardLineChart.js
+++ src/components/Cards/CardLineChart.js
@@ -13,9 +13,7 @@ export default function CardLineChart() {
           "April",
           "May",
           "June",
-          "July",
-          "Aug",
-          "Sep"
+          "July"
         ],
         datasets: [
           {
@@ -117,7 +115,7 @@ export default function CardLineChart() {
               <h6 className="uppercase text-blueGray-100 mb-1 text-xs font-semibold">
                 Overview
               </h6>
-              <h2 className="text-white text-xl font-semibold">Sales valueeeeeeee PD</h2>
+              <h2 className="text-white text-xl font-semibold">Sales valueeeeee PDuyyy</h2>
             </div>
           </div>
         </div>

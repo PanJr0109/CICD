4cc7ed63c79f7928fdcf6abfe320f85198e58ae1
20120068 <phanduy0000123@gmail.com>
2024-12-12 01:35:33 +0700

    init

--- src/components/Cards/CardLineChart.js
+++ src/components/Cards/CardLineChart.js
@@ -12,7 +12,7 @@ export default function CardLineChart() {
           "March",
           "April",
           "May",
-          "Juneeeeeeeêsdfasdfasdfasdfasdfasdfà",
+          "June",
         ],
         datasets: [
           {
@@ -112,9 +112,9 @@ export default function CardLineChart() {
           <div className="flex flex-wrap items-center">
             <div className="relative w-full max-w-full flex-grow flex-1">
               <h6 className="uppercase text-blueGray-100 mb-1 text-xs font-semibold">
-                Overview Chart
+                Overview
               </h6>
-              <h2 className="text-white text-xl font-semibold">Salessssssssssssss value</h2>
+              <h2 className="text-white text-xl font-semibold">Sales value</h2>
             </div>
           </div>
         </div>

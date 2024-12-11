f89bf073f095c09502f051fb0bf4809e5b362b13
20120068 <phanduy0000123@gmail.com>
2024-12-12 00:15:52 +0700

    klkl

--- src/components/Cards/CardBarChart.js
+++ src/components/Cards/CardBarChart.js
@@ -13,7 +13,8 @@ export default function CardBarChart() {
           "April",
           "May",
           "June",
-          "July"
+          "July",
+          "klklkl"
         ],
         datasets: [
           {

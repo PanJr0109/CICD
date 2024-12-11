f7520fcd277b61a4ebe389b72e053b584902fc83
20120068 <phanduy0000123@gmail.com>
2024-12-12 01:33:58 +0700

    qua met

--- src/components/Cards/CardLineChart.js
+++ src/components/Cards/CardLineChart.js
@@ -114,7 +114,7 @@ export default function CardLineChart() {
               <h6 className="uppercase text-blueGray-100 mb-1 text-xs font-semibold">
                 Overview Chart
               </h6>
-              <h2 className="text-white text-xl font-semibold">Sales value</h2>
+              <h2 className="text-white text-xl font-semibold">Salessssssssssssss value</h2>
             </div>
           </div>
         </div>

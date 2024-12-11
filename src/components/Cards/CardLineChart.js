eda8869835105fb610e4f87cb8d28e0147b74bab
20120068 <phanduy0000123@gmail.com>
2024-12-12 02:48:08 +0700

    change

--- src/components/Cards/CardLineChart.js
+++ src/components/Cards/CardLineChart.js
@@ -13,7 +13,7 @@ export default function CardLineChart() {
           "April",
           "May",
           "June",
-          "July"
+          "July",
         ],
         datasets: [
           {
